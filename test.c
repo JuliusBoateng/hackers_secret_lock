1572f05ee518376be724cb88b9c31ec792c007ea
Stephen Hannon <stephenhannon4@gmail.com>
2021-02-16 15:24:55 -0500

    update testing

--- test.c
+++ test.c
@@ -1,3 +1,8 @@
+/* Macros are flexible with whitespace */
+   #define   a'a'
+   #define b\
+'b'
+
 /* C is flexible with newlines */
 int
 main
@@ -11,7 +16,42 @@ char
 argv
 )
 {
-return
-0
-;
+	putchar(a);
+	putchar(b);
+
+	/* Use other bases instead of decimal */
+	putchar(0x63); /* c */
+	putchar(0144); /* d */
+
+	/* Use numbers instead of chars */
+	putchar(10);
+
+	/* Ideas for removable locking part */
+	int lock;
+	lock =
+		!!!
+		!!!
+		!!!
+1;
+	puts(lock ? "lock\n" : "unlock\n");
+
+	int DEL = 0;
+	lock =
+1;
+	puts(lock ? "lock\n" : "unlock\n");
+
+	/* Strings next to each other are concatenated */
+	puts("a"
+"b");
+
+	/* Use char arithmetic to obfuscate strings */
+	/* Prints "Hello!\n" */
+	char *s = "Mionp!\t";
+	int i = -5;
+	for(; *s; s++) {
+		putchar(i+++*s);
+	}
+	/* i is now 2, and can be used elsewhere */
+
+	return 0;
 }
