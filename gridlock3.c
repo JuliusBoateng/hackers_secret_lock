9fddf63377c916094a015d4ddd756f5cdf700241
Stephen Hannon <stephenhannon4@gmail.com>
2021-02-23 21:56:43 -0500

    Tweak

--- gridlock3.c
+++ gridlock3.c
@@ -15,7 +15,7 @@ t,a,j=0,b=c*9-1;for(c=0;k[c];c++);
 for(t=k;*t;t++){if(U(*t-0x20))*t-=
 040;if(!U(*t))return!f(0);}t=*++v;
 for(;*t;t++){a=U(*t)?0x41:U(*t-32)
-?97:ME;a&&(*t=a+((*t-a)+s*(k[j++%c
+?0141:ME;a&&(*t=a+(*t-a+s*(k[j++%c
 ]-0101)+b)%b);}return!puts(*v);}f(
 x){char*s="R5k5~l$vplwftmto\037b`"
 "ib\x1d\x1cgg`_sm\x19ll\\\\hnhi\0"
